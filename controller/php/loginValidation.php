8be1925c3a585d8a050316279a5bce2dde009b02
rahmanMunna <habiburmunna0@gmail.com>
2025-05-20 00:29:47 +0600

    login signup page validated by php

--- controller/php/loginValidation.php
+++ controller/php/loginValidation.php
@@ -1,4 +1,47 @@
 <?php
+
+function isAllLowerCase($text)
+{
+    return strtolower($text) === $text;
+}
+
+function isFirstCharacterNumber($text)
+{
+    return is_numeric($text[0]);
+}
+
+function isValidPassword($password)
+{
+    if (strlen($password) < 8) return false;
+
+    $hasUpper = false;
+    $hasLower = false;
+    $hasSpecial = false;
+    $hasNumber = false;
+
+    $specialChars = "!@#$%&*()_+[]{}|<>?/";
+
+    for ($i = 0; $i < strlen($password); $i++) {
+        $char = $password[$i];
+
+        if ($char >= 'A' && $char <= 'Z') {
+            $hasUpper = true;
+        } elseif ($char >= 'a' && $char <= 'z') {
+            $hasLower = true;
+        } elseif ($char >= '0' && $char <= '9') {
+            $hasNumber = true;
+        } elseif (strpos($specialChars, $char) !== false) {
+            $hasSpecial = true;
+        }
+
+        if ($hasUpper && $hasLower && $hasNumber && $hasSpecial) {
+            return true;
+        }
+    }
+
+    return $hasUpper && $hasLower && $hasNumber && $hasSpecial;
+}
+
 session_start();
 if (isset($_POST["submit"])) {
 
@@ -7,20 +50,22 @@ if (isset($_POST["submit"])) {
     $userId = $loginData["userId"];
     $password = $loginData["password"];
 
+    // admin fake data
+
     $guestEmail = "guest@gmail.com";
     $guestPassword = "1234";
     $guestID = '1234';
 
+    //guest fake
     $adminEmail = "munna@gmail.com";
     $adminPassword = "1234";
     $adminID = '1234';
 
-    // echo "name : {$email} password : ${password}";
+   
 
     if ($email == $adminEmail && $userId == $adminID && $password = $adminPassword) {
 
-        // $_SESSION['userId'] = $userId;
-        // $_SESSION['email'] = $email;
+
         $_SESSION['status'] = true;
         setcookie('status', 'true', time() + 5000, '/');
         header('Location: ../../View/Dashboard/admin.php');
--- controller/php/signup.php
+++ controller/php/signup.php
@@ -1,6 +1,6 @@
 <?php
-
 if (isset($_POST['submit'])) {
+
     $firstName = trim($_POST["firstName"] ?? '');
     $lastName = trim($_POST["lastName"] ?? '');
     $email = trim($_POST["email"] ?? '');
@@ -9,94 +9,100 @@ if (isset($_POST['submit'])) {
     $password = $_POST["password"] ?? '';
     $confirmPassword = $_POST["confirmPassword"] ?? '';
 
-    // Redirect helper
-    // function redirectBack()
-    // {
-    //     header('Location: ../View/Authentication/SignUp/signup.html');
-    // }
+    //Redirect to sign up page
+    function redirectBack()
+    {
+        header('Location: ../View/Authentication/SignUp/signup.html');
+    }
 
-    // // Check for empty fields
-    // if (
-    //     $firstName === '' || $lastName === '' || $email === '' ||
-    //     $phone === '' || $dob === '' || $password === '' || $confirmPassword === ''
-    // ) {
-    //     redirectBack();
-    // }
+    // Check for empty fields
+    if (
+        $firstName === '' || $lastName === '' || $email === '' ||
+        $phone === '' || $dob === '' || $password === '' || $confirmPassword === ''
+    ) {
+        redirectBack();
+    }
 
-    // // Email validation
-    // if (strpos($email, '@') === false) {
-    //     redirectBack();
-    // }
+    // Email validation
+    if (strpos($email, '@') === false) {
+        redirectBack();
+    }
 
-    // $emailParts = explode('@', $email, 2);
-    // if (count($emailParts) !== 2) {
-    //     redirectBack();
-    // }
+    $emailParts = explode('@', $email, 2);
+    if (count($emailParts) !== 2) {
+        redirectBack();
+    }
 
-    // list($localPart, $domain) = $emailParts;
+    list($localPart, $domain) = $emailParts;
 
-    // if ($domain !== 'gmail.com') {
-    //     redirectBack();
-    // }
+    if ($domain !== 'gmail.com') {
+        redirectBack();
+    }
 
-    // if ($localPart !== strtolower($localPart)) {
-    //     redirectBack();
-    // }
+    if ($localPart !== strtolower($localPart)) {
+        redirectBack();
+    }
 
-    // $firstChar = $localPart[0] ?? '';
-    // if ($firstChar >= '0' && $firstChar <= '9') {
-    //     redirectBack();
-    // }
+    $firstChar = $localPart[0] ?? '';
+    if ($firstChar >= '0' && $firstChar <= '9') {
+        redirectBack();
+    }
 
-    // if (strpos($localPart, ' ') !== false) {
-    //     redirectBack();
-    // }
+    if (strpos($localPart, ' ') !== false) {
+        redirectBack();
+    }
 
-    // // Password validation
-    // function isValidPassword($password)
-    // {
-    //     if (strlen($password) < 8) return false;
+    // Password validation
+    function isValidPassword($password)
+    {
+        if (strlen($password) < 8) return false;
 
-    //     $hasUpper = false;
-    //     $hasLower = false;
-    //     $hasSpecial = false;
-    //     $hasNumber = false;
+        $hasUpper = false;
+        $hasLower = false;
+        $hasSpecial = false;
+        $hasNumber = false;
 
-    //     $specialChars = "!@#$%&*()_+[]{}|<>?/";
+        $specialChars = "!@#$%&*()_+[]{}|<>?/";
 
-    //     for ($i = 0; $i < strlen($password); $i++) {
-    //         $char = $password[$i];
+        for ($i = 0; $i < strlen($password); $i++) {
+            $char = $password[$i];
 
-    //         if ($char >= 'A' && $char <= 'Z') $hasUpper = true;
-    //         else if ($char >= 'a' && $char <= 'z') $hasLower = true;
-    //         else if (strpos($specialChars, $char) !== false) $hasSpecial = true;
-    //         else if ($char >= '0' && $char <= '9') $hasNumber = true;
+            if ($char >= 'A' && $char <= 'Z') $hasUpper = true;
+            else if ($char >= 'a' && $char <= 'z') $hasLower = true;
+            else if (strpos($specialChars, $char) !== false) $hasSpecial = true;
+            else if ($char >= '0' && $char <= '9') $hasNumber = true;
 
-    //         if ($hasUpper && $hasLower && $hasSpecial && $hasNumber) {
-    //             return true;
-    //         }
-    //     }
+            if ($hasUpper && $hasLower && $hasSpecial && $hasNumber) {
+                return true;
+            }
+        }
 
-    //     return false;
-    // }
+        return false;
+    }
 
-    // if (!isValidPassword($password)) {
-    //     redirectBack();
-    // }
+    if (!isValidPassword($password)) {
+        redirectBack();
+    }
 
-    // // Confirm password check
-    // if ($password !== $confirmPassword) {
-    //     redirectBack();
-    // }
+    // Confirm password check
+    if ($password !== $confirmPassword) {
+        redirectBack();
+    }
 
-    // // Phone validation
-    // if (strlen($phone) !== 11) {
-    //     redirectBack();
-    // }
+    // Phone validation
+    if (strlen($phone) !== 11) {
+        redirectBack();
+    }
 
-    // if ($phone[0] !== '0' || $phone[1] !== '1') {
-    //     redirectBack();
-    // }
+    if ($phone[0] !== '0' || $phone[1] !== '1') {
+        redirectBack();
+    }
+
+    //Set success cookie and redirect
+    $_SESSION['status'] = true;
+    setcookie('status', 'true', time() + 5000, '/');
+    header('../../View/Dashboard/guest_dashboard.php');
+    exit();
 
     // $firstName = trim($_POST["firstName"] ?? '');
     // $lastName = trim($_POST["lastName"] ?? '');
@@ -106,7 +112,7 @@ if (isset($_POST['submit'])) {
     // $password = $_POST["password"] ?? '';
     // $confirmPassword = $_POST["confirmPassword"] ?? '';
 
-    $con = mysqli_connect('127.0.0.1', 'root', '', 'hotel-reservation');
+    // $con = mysqli_connect('127.0.0.1', 'root', '', 'hotel-reservation');
     // print_r($con);
 
     // $sql = 'select * from users'; // write query
@@ -123,7 +129,7 @@ if (isset($_POST['submit'])) {
 
 
     // Set success cookie and redirect
-    $_SESSION['status'] = true;
+    // $_SESSION['status'] = true;
     // setcookie('status', 'true', time() + 5000, '/');
     // header('Location: ../View/home.html');
     // exit;
@@ -134,21 +140,24 @@ if (isset($_POST['submit'])) {
 
 ?>
 
+
+
+
 <?php
 
-function insertToDb($connection, $firstName, $lastName, $email, $phone, $dob, $password, $role, $userId)
-{
-    $sql = "INSERT INTO users VALUES (null, '$firstName', '$lastName', '$email', '$phone', '$dob', '$password', '$role', '$userId')";
+// function insertToDb($connection, $firstName, $lastName, $email, $phone, $dob, $password, $role, $userId)
+// {
+//     $sql = "INSERT INTO users VALUES (null, '$firstName', '$lastName', '$email', '$phone', '$dob', '$password', '$role', '$userId')";
 
-    if (mysqli_query($connection, $sql)) {
-        echo "inserted!";
-    } else {
-        echo "DB error!";
-    }
-}
+//     if (mysqli_query($connection, $sql)) {
+//         echo "inserted!";
+//     } else {
+//         echo "DB error!";
+//     }
+// }
 
-function readData($connection,$query) {
-    
-}
+// function readData($connection,$query) {
+
+// }
 
 ?>
